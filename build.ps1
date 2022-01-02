d3c3e849d40ab6995890710e4d758ca64432576f
Christian Thillemann <christianthillemann@gmail.com>
2022-01-02 23:50:15 +0100

    remove verbose stuff in powershell script

--- build.ps1
+++ build.ps1
@@ -6,16 +6,12 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
-Write-Host "running dotnet tool restore"
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) {
-    Write-Host "error dotnet tool restore"
      exit $LASTEXITCODE
 }
 
-Write-Host "running dotnet cake"
 dotnet cake @args
 if ($LASTEXITCODE -ne 0) {
-    Write-Host "error dotnet tool restore"
     exit $LASTEXITCODE
 }
