6290edd07d457fb9f54d6c22bd8851c74b100a48
Christian Thillemann <christianthillemann@gmail.com>
2022-01-02 23:45:20 +0100

    add verbose stuff

--- build.ps1
+++ build.ps1
@@ -6,8 +6,16 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
+Write-Host "running dotnet tool restore"
 dotnet tool restore
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "error dotnet tool restore"
+     exit $LASTEXITCODE
+}
 
+Write-Host "running dotnet cake"
 dotnet cake @args
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "error dotnet tool restore"
+    exit $LASTEXITCODE
+}
